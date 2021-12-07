bbc12d69437c7bccdc4c6e8d856765b965517176
tyson-01 <92799225+tyson-01@users.noreply.github.com>
2021-12-07 19:14:52 -0400

    Update home.js

--- www/js/home.js
+++ www/js/home.js
@@ -7,3 +7,18 @@ document.getElementById("drop-menu").onclick = function() {
         mNav.style.display = "none";
     }
 }
+
+var contactButton = document.getElementById('send-btn');
+var messageArea1 = document.getElementById("message-box1");
+var messageArea2 = document.getElementById("message-box2");
+var messageArea3 = document.getElementById("message-box3");
+contactButton.onclick = function() {
+    var fName = document.getElementById('first-name').value;
+    var lName = document.getElementById("last-name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    alert("Gracias");
+    messageArea1.innerHTML = "Name: " + fName + " " + lName;
+    messageArea2.innerHTML = "Email: " + email;
+    messageArea3.innerHTML = "Message: " + message;
+}
